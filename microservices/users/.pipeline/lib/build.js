b5306de13a44f32b6446c7ca7aff369d9a57fe5f
poornima-sivanand <poornima.sivanand@gov.bc.ca>
2022-06-07 15:05:06 -0700

    add pipeline cli

---
+++ microservices/users/.pipeline/lib/build.js
@@ -0,0 +1,26 @@
+'use strict';
+const {OpenShiftClientX} = require('@bcgov/pipeline-cli')
+const path = require('path');
+
+module.exports = (settings)=>{
+  const phases=settings.phases
+  const options = settings.options
+  const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
+  const phase='build'
+  var objects = []
+
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
+
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/build.yaml`, {
+    'param':{
+      'NAME': `${phases[phase].name}-server`,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'GIT_URL': oc.git.url,
+      'GIT_BRANCH': oc.git.branch.name,
+    }
+  }));
+
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
+  oc.applyAndBuild(objects)
+}
